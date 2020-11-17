24c2ade3573ae7dd7f312e24622f066905e3cb8e
namuan <575441+namuan@users.noreply.github.com>
2020-11-17 06:54:16 +0000

    Add new functions to calculate series cross-overs and a new strategy

--- app/common/__init__.py
+++ app/common/__init__.py
@@ -1,5 +1,7 @@
 import json
 import uuid
+import pandas as pd
+import numpy as np
 
 UTF_ENCODING = "utf-8"
 
@@ -24,3 +26,32 @@ def uuid_gen():
 
 def candle_event_name(exchange_id, timeframe):
     return "{}-ohlcv-{}".format(exchange_id, timeframe)
+
+
+def crossed(series1, series2, direction=None):
+    if isinstance(series1, np.ndarray):
+        series1 = pd.Series(series1)
+
+    if isinstance(series2, (float, int, np.ndarray, np.integer, np.floating)):
+        series2 = pd.Series(index=series1.index, data=series2)
+
+    if direction is None or direction == "above":
+        above = pd.Series((series1 > series2) & (
+                series1.shift(1) <= series2.shift(1)))
+
+    if direction is None or direction == "below":
+        below = pd.Series((series1 < series2) & (
+                series1.shift(1) >= series2.shift(1)))
+
+    if direction is None:
+        return above or below
+
+    return above if direction == "above" else below
+
+
+def crossed_above(series1, series2):
+    return crossed(series1, series2, "above")
+
+
+def crossed_below(series1, series2):
+    return crossed(series1, series2, "below")
--- app/strategies/__init__.py
+++ app/strategies/__init__.py
@@ -1,8 +1,10 @@
 from app.config.service_locators import locator
 from app.strategies.simple_strategy import SimpleStrategy
 from app.strategies.ma_xo_strategy import MaCrossOverStrategy
+from app.strategies.ema_bb_alligator_strategy import EMABBAlligatorStrategy
 
 all_strategies = [
     # SimpleStrategy(locator),
-    MaCrossOverStrategy(locator)
+    MaCrossOverStrategy(locator),
+    EMABBAlligatorStrategy(locator)
 ]
